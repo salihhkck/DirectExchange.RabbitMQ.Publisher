87c39139cfcf3ce02abb74d47ab0c9d60e961d2a
Salih Kucuk <salihkucukdeveloper@gmail.com>
2024-01-29 23:06:52 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace DirectExchange.RabbitMQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bağlantı Oluşturma RabbitMQ ile.
+            ConnectionFactory connectionFactory = new();
+            connectionFactory.Uri = new("amqps://rkjhqugu:ldW9uYt3EtaPRzn9_ulmFp657F27TyNA@jackal.rmq.cloudamqp.com/rkjhqugu");
+
+
+            //Bağlantı aktifleştirme ve kanal açma
+            using IConnection connection = connectionFactory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+            while (true)
+            {
+                Console.WriteLine("Mesaj : ");
+                string message=Console.ReadLine();
+                byte[] byteMessage=Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(
+                    exchange: "direct-exchange-example",
+                    routingKey: "direct-exchange-example",
+                    body:byteMessage);
+            }
+
+            Console.Read();
+        }
+    }
+}
